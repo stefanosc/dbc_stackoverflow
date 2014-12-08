ce586253f2fc6701acfa558518d52dbca86b4423
Stefano Schiavi <stefano@bvprojects.org>
2014-12-08 15:15:03 -0800

    add question / answer associations and specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208225157) do
+ActiveRecord::Schema.define(version: 20141208231234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,11 @@ ActiveRecord::Schema.define(version: 20141208225157) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "question_id"
   end
 
   add_index "answers", ["content"], name: "index_answers_on_content", using: :btree
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
   add_index "answers", ["title"], name: "index_answers_on_title", using: :btree
 
   create_table "questions", force: true do |t|
