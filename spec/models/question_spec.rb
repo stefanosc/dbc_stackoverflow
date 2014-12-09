dc3c85485aa5281d602ffde27fd5a9a8ba83bc53
Stefano Schiavi <stefano@bvprojects.org>
2014-12-08 21:45:36 -0800

    add partial to render flash messages

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Question, :type => :model do
   it { should have_many(:answers) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:content) }
 end
