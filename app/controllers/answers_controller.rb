3b1ed745efec69f165d9e0aea61388be2702f34d
Stefano Schiavi <stefano@bvprojects.org>
2014-12-08 21:58:28 -0800

    complete answers#create action and spec

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -9,7 +9,19 @@ class AnswersController < ApplicationController
   end
 
   def create
-    @answer = Answer.new(answer_params)
+    @question = find_question_from_params
+    @answer = @question.answers.build(answer_params)
+    if @answer.save
+      redirect_to question_path(@question)
+      flash[:success] = "Successfully created answer"
+    else
+      @answers = @question.answers
+      flash[:danger] = ""
+      @answer.errors.full_messages.each do |msg|
+        flash[:danger] << msg + ", "
+      end
+      render "questions/show"
+    end
   end
 
   def edit
@@ -25,5 +37,9 @@ class AnswersController < ApplicationController
 
   def answer_params
     params.require(:answer).permit(:title,:content)
+  end
+
+  def find_question_from_params
+    Question.find_by(id: params[:question_id])
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,40 +2,57 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, :type => :controller do
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 
 
   describe "POST create" do
+    context "with valid attributes" do
+      let(:question) { Question.create(title: "question title", content: "content") }
+      let(:attrs) { {title: "title", content: "answer content"} }
+      before { post :create, {question_id: question.id, answer: attrs} }
 
-    let(:question) { Question.create(title: "question title", content: "content") }
-    let(:attrs) { {title: "title", content: "answer content"} }
-    before { post :create,  answer: attrs }
+      it "saves the answer to the database" do
+        expect(Answer.count).to eq(1)
+      end
+      it "saves the answer associated to the question" do
+        expect(question.answers.count).to eq(1)
+        expect(question.answers.last.title).to eq(attrs[:title])
+      end
+      it "redirects question_path" do
+        expect(response).to redirect_to question_path(question)
+      end
+      it "sets a flash success message" do
+        expect(flash[:success]).not_to be nil
+      end
+    end
 
-    it "saves the answer to the database" do
-      expect(Answer.count).to eq(1)
-    end
-    it "saves the answer associated to the question" do
-      expect(question.answers.count).to eq(1)
-      expect(question.answers.last.title).to eq(attrs[:title])
-    end
-    it "redirects questions_path" do
-      expect(response).to redirect_to question_path(question)
-    end
-    it "sets a flash success message" do
-      expect(flash[:success]).not_to be nil
+    context "with invalid attributes" do
+      let(:question) { FactoryGirl.create(:question) }
+      let(:attrs) { FactoryGirl.attributes_for(:invalid_answer, question: question) }
+      before { post :create, {question_id: question.id, answer: attrs} }
+
+      it "does NOT save the answer to the database" do
+        expect(Answer.count).to eq(0)
+      end
+      it "renders the question/show template" do
+        expect(response).to render_template("questions/show")
+      end
+      it "sets a flash success message" do
+        expect(flash[:danger]).not_to be nil
+      end
     end
   end
 
